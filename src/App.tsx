bc06813b57c490368bd0f79ba8132f57fc27df2d
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-08-31 19:58:22 +0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Details from "./pages/Details";
 import Home from "./pages/Home";
+import './index.css';
 
 function App() {
     return (
