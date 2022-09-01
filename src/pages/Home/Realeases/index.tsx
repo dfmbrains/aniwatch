e3f7000d785299f8558375efeb4e5d2843159f29
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-09-01 19:23:07 +0600

    commit for maks

--- src/pages/Home/Realeases/index.tsx
+++ src/pages/Home/Realeases/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Releases.scss";
+import axios from "axios";
 import {Swiper, SwiperSlide} from "swiper/react";
 
 
 const Realeases = () => {
 
-    const arr = [1,2,3,4,5,6,7,8,9,10];
+    const [topList, setTopList] = useState([]);
+
+    useEffect(()=>{
+        const options = {
+            method: 'GET',
+            url: 'https://jikan1.p.rapidapi.com/top/anime/1/upcoming',
+            headers: {
+                'X-RapidAPI-Key': '69ceed32c9msh526975afd6f4d95p10cc17jsn195dc89bbd83',
+                'X-RapidAPI-Host': 'jikan1.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            setTopList(response.data.top);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }, []);
 
     return (
         <div className="realeases">
@@ -14,11 +32,15 @@ const Realeases = () => {
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={6}>
-                    {arr
-                        ? arr.map((item: number)=>(
+                    {topList.length > 0
+                        ? topList.map((item: any)=>(
                             <SwiperSlide key={item}>
                                 <div className="realeases__card">
-
+                                    <img className="realeases__card_img" src={item.image_url} alt=""/>
+                                    <div className="realeases__card_blur">
+                                    </div>
+                                    <span className="realeases__card_subtitle">{item.start_date}</span>
+                                    <span className="realeases__card_title">{item.title}</span>
                                 </div>
                             </SwiperSlide>
                         ))
